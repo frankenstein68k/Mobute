89d3cb1962fd5f3a75dab2f4af8656cf3b6046bb
David Piggott <djmpiggott@Davids-MacBook-Pro.local>
2015-04-12 14:44:14 -0400

    Table View Segue to Detail View Added

---
+++ Mobute/Mobute/JobDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JobDetailViewController.swift
+//  Mobute
+//
+//  Created by David Piggott on 2015-04-12.
+//  Copyright (c) 2015 Neil MacKenzie. All rights reserved.
+//
+
+import UIKit
+
+class JobDetailViewController: UIViewController {
+    
+    
+    @IBOutlet var jobTitleLabel: UILabel!
+    
+    var jobTitle = String()
+    
+    override func viewWillAppear(animated: Bool) {
+        jobTitleLabel.text = jobTitle
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
--- Mobute/Mobute/SecondViewController.swift
+++ Mobute/Mobute/SecondViewController.swift
@@ -8,41 +8,56 @@
 
 import UIKit
 
-class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    @IBOutlet
-    var tableView: UITableView!
-    var items: [String] = ["data1", "data2", "data3"]
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
+    let jobData = ["Job 1", "Job 2", "Job 3", "Job 4", "Job 5", "Job 6", "Job 7", "Job 8", "Job 9", "Job 10", "Job 11", "Job 12", "Job 13", "Job 14"]
+    
+    let textCellIdentifier = "TextCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        return self.items.count;
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jobData.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
         
-        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-        
-        cell.textLabel?.text = self.items[indexPath.row]
+        let row = indexPath.row
+        cell.textLabel?.text = jobData[row]
         
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
-        println("You selected cell!")
-        
+        let row = indexPath.row
+        println(jobData[row])
+    }
+    
+    let jobSegueIdentifier = "ShowJobSegue"
+    
+    // MARK: - Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == jobSegueIdentifier {
+            if let destination = segue.destinationViewController as? JobDetailViewController {
+                if let jobIndex = tableView.indexPathForSelectedRow()?.row {
+                    destination.jobTitle = jobData[jobIndex]
+                }
+            }
+        }
     }
     
 
